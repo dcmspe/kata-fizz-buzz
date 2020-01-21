31b445b315148bae93797a426b3ad00efdde4a46
dcmspe <dcms.pe@gmail.com>
2020-01-21 11:39:06 +0000

    Adding fail stament to init the tdd.

--- src/app/app.fizzbuzz.spec.ts
+++ src/app/app.fizzbuzz.spec.ts
@@ -1,5 +1,7 @@
 import { FizzBuzz } from './app.fizzbuzz';
 
 describe('FizzBuzz', () => {
-  
+  it("shoud make a fizzbuzz instance", function(){
+      fail("Make this test fail first to learn TDD");
+  });
 });
