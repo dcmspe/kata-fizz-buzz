5b5b77467d306182d1c2b88673dc6192a6177696
dcmspe <dcms.pe@gmail.com>
2020-01-21 11:06:49 +0000

    Good answer.

--- src/app/app.fizzbuzz.spec.ts
+++ src/app/app.fizzbuzz.spec.ts
@@ -4,10 +4,51 @@ describe('FizzBuzz', () => {
 
   it('should create a fiz buzz instance', () => {
 
-    //var fizzBuzz = new FizzBuzz();
+    var fizzBuzz = new FizzBuzz();
 
-    //expect(fizzBuzz).not.toBe(null);
+    expect(fizzBuzz).not.toBe(null);
 
   });
 
+  it("should make fizzbuzz says when a value is multiple of 3", function(){
+    var fizzBuzz = new FizzBuzz();
+
+    expect(fizzBuzz.isMultipleOf3(6)).toBe(true);
+  });
+
+  it("should make fizzbuzz says when a value is multiple of 5", function(){
+    var fizzBuzz = new FizzBuzz();
+
+    expect(fizzBuzz.isMultipleOf5(10)).toBe(true);
+  });
+
+  it("should make fizzbuzz says when a value is multiple of 3 & 5", function(){
+    var fizzBuzz = new FizzBuzz();
+
+    expect(fizzBuzz.isMultipleOf3and5(15)).toBe(true);
+  });
+
+  it("should make fizzbuzz says fizz when a value is multiple of 3", function(){
+    var fizzBuzz = new FizzBuzz();
+
+    expect(fizzBuzz.sendValue(3)).toBe("fizz");
+  });
+
+  it("should make fizzbuzz says buzz when a value is multiple of 5", function(){
+    var fizzBuzz = new FizzBuzz();
+
+    expect(fizzBuzz.sendValue(5)).toBe("buzz");
+  });
+
+  it("should make fizzbuzz says fizzbuzz when a value is multiple of 3&5", function(){
+    var fizzBuzz = new FizzBuzz();
+
+    expect(fizzBuzz.sendValue(15)).toBe("fizzbuzz");
+  });
+
+  it("should give array and expect the array to contains fizz, buzz, fizzbuzz", function(){
+    var fizzBuzz = new FizzBuzz();
+    var array = fizzBuzz.sendArray([1,3,15,20]);
+    expect(array).toEqual([1,"fizz","fizzbuzz","buzz"]);
+  });
 });
--- src/app/app.fizzbuzz.ts
+++ src/app/app.fizzbuzz.ts
@@ -4,4 +4,39 @@ export class FizzBuzz{
 
     }
 
+    isMultipleOf3(value:number){
+      return value % 3 == 0;
+    }
+
+    isMultipleOf5(value:number){
+      return value % 5 == 0;
+    }
+
+
+    isMultipleOf3and5(value:number){
+      return this.isMultipleOf3(value) && this.isMultipleOf5(value);
+    }
+
+
+    sendValue(value:number){
+
+      if (this.isMultipleOf3and5(value)){
+        return "fizzbuzz";
+      } else if (this.isMultipleOf5(value)){
+        return "buzz";
+      } else if (this.isMultipleOf3(value)){
+        return "fizz";
+      } else {
+        return value;
+      }
+    }
+
+    sendArray(array:Array<any>){
+      var index = 0;
+      array.forEach(value =>  {
+        array[index]=this.sendValue(value);
+        index++;
+      });
+      return array;
+    }
 }
